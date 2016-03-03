6dba8ba97e258e85ddc19e8f14e18278ec7c7b5f
Chao Feng <fengc1@niksula.hut.fi>
2016-03-03 16:35:28 +0200

    maven used. OpenCV-Android:3.1.0

--- app/src/main/java/fi/aalto/parrot/ardrone/drone/MainActivity.java
+++ app/src/main/java/fi/aalto/parrot/ardrone/drone/MainActivity.java
@@ -1,13 +1,30 @@
 package fi.aalto.parrot.ardrone.drone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startRecognitionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startRecognitionButton = (Button) findViewById(R.id.recognition);
+
+        startRecognitionButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RecognitionActivity.class));
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/fi/aalto/parrot/ardrone/drone/RecognitionActivity.java
@@ -0,0 +1,14 @@
+package fi.aalto.parrot.ardrone.drone;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RecognitionActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recognition);
+    }
+}
